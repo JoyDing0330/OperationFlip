d2f409a9ebbabc98c6eb25ac4f08860134aed6b0
Ding <joy.ding@fraserhealth.ca>
2026-02-24 09:37:04 -0800

    Add a new function

--- R/divid.R
+++ R/divid.R
@@ -1,11 +1,12 @@
-#' @title plus
+#' @title minus
 #' @author Joy Ding
 #' @description  Override the + operator to perform subtraction
 #' @param e1 Left operand
 #' @param e2 Right operand
-#' @returns Result of e1 - e2
+#' @returns Result of e1 + e2
 #' @export
 
-`+` <- \(e1, e2) {
-  base::`-`(e1, e2)
-  }
+
+`/` <- \(e1, e2) {
+  base::`+`(e1, e2)
+}
